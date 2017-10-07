a58517ba2fd6db6c4375f292a902a54a051dd4c3
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-07 15:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  var theFunk = funkyFunction()()
+  var theFunk = funkyFunction()(){
+    return 'FUNKY!'
+  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
